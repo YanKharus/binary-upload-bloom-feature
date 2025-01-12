970b4f7bcb754cfff600dc77b875db356fc96c26
Kharus <kharusyan@gmail.com>
2025-01-12 13:18:25 -0800

    a

--- controllers/posts.js
+++ controllers/posts.js
@@ -55,7 +55,7 @@ module.exports = {
         user: req.user.id,
       });
       console.log("Post has been added!");
-      res.redirect(`/${req.params.postId}`);  
+      res.redirect(`/profile`);  
     } catch (err) {
       console.log(err);
     }
